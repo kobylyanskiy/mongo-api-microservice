7dc3d0c5655631661b51ab72a70672de348cae2f
Kirill Kobylyanskiy <kirill.kobylyanskiy@emlid.com>
2018-05-05 22:03:20 +0300

    CD: integrate with TravisCI

--- app.py
+++ app.py
@@ -8,13 +8,28 @@ app = Flask(__name__)
 @app.route('/agents', methods=['GET', 'POST'])
 def agents():
     if request.method == 'POST':
+        agent = request.get_json(silent=True)
+        print(agent)
         return json.dumps({
             'result': True,
         })
     else:
         return json.dumps({
             'result': True,
-            'data': [{'codename': 'agent'}],
+            'data': [],
+        })
+
+
+@app.route('/agents/<string:codename>', methods=['GET', 'POST'])
+def get_agent(codename):
+    if request.method == 'POST':
+        agent = request.get_json(silent=True)
+        print(agent)
+        return json.dumps({'result': True})
+    else:
+        return json.dumps({
+            'result': True,
+            'agent': {'codename': 'codename'}
         })
 
 
--- update-service.sh
+++ update-service.sh
@@ -6,7 +6,19 @@ VERSION=$1
 PROJECT_ID=spy-crowd
 
 ${DOCKER} build -t ${IMAGE_LOCAL_PREFIX}/${DEPLOYMENT}:${VERSION} .
+
+echo $GCLOUD_SERVICE_KEY_PRD | base64 --decode -i > ${HOME}/gcloud-service-key.json
+gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
+
+gcloud --quiet config set project $PROJECT_NAME_PRD
+gcloud --quiet config set container/cluster $CLUSTER_NAME_PRD
+gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
+gcloud --quiet container clusters get-credentials $CLUSTER_NAME_PRD
+
 ${DOCKER} tag ${IMAGE_LOCAL_PREFIX}/${DEPLOYMENT}:${VERSION} gcr.io/${PROJECT_ID}/${DEPLOYMENT}:${VERSION}
+
+docker login -u _json_key --password-stdin https://gcr.io < ${HOME}/gcloud-service-key.json
+
 ${DOCKER} push gcr.io/${PROJECT_ID}/${DEPLOYMENT}:${VERSION}
 
 ${KUBECTL} set image deployment/${DEPLOYMENT}-deployment ${DEPLOYMENT}=gcr.io/${PROJECT_ID}/${DEPLOYMENT}:${VERSION}
